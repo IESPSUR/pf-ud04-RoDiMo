d95d1b02ff3b1be0e4f27d790cf87df1eabefb76
RoDiMo <roberto.diaz.morffi.al@iespoligonosur.org>
2022-10-31 18:39:29 +0100

    Añadiendo Modelos

--- tienda/admin.py
+++ tienda/admin.py
@@ -4,3 +4,4 @@ from .models import *
 # Register your models here.
 admin.site.register(Marca)
 admin.site.register(Producto)
+admin.site.register(Compra)
--- tienda/models.py
+++ tienda/models.py
@@ -7,9 +7,19 @@ class Marca(models.Model):
 
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length=60)
-    modelo = models.CharField(max_length=60)
+    nombre = models.CharField(max_length=60, unique=True)
+    modelo = models.CharField(max_length=60, unique=True)
     unidades = models.IntegerField()
     precio = models.FloatField();
     detalles = models.CharField(max_length=100)
     marca = models.ForeignKey('Marca', on_delete=models.CASCADE)
+
+
+class Compra(models.Model):
+    fecha = models.DateTimeField()
+    importe = models.FloatField()
+    unidades = models.IntegerField()
+    nombre = models.ForeignKey('Producto', to_field='nombre', related_name='Nombre', on_delete=models.CASCADE)
+    modelo = models.ForeignKey('Producto', to_field='modelo', related_name='Modelo', on_delete=models.CASCADE)
+
+
