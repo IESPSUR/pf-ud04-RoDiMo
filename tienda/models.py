ab8f16919d200de23b1dcc7ff13366f23609b3af
RoDiMo <roberto.diaz.morffi.al@iespoligonosur.org>
2022-10-31 17:17:10 +0100

    Añadiendo Modelos

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=60, primary_key=True)
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=60)
+    modelo = models.CharField(max_length=60)
+    unidades = models.IntegerField()
+    precio = models.FloatField();
+    detalles = models.CharField(max_length=100)
+    marca = models.ForeignKey('Marca', on_delete=models.CASCADE)
